bab55db16763628cb176f9f81a1fc7390a844638
kleitomberg <kleitom-1@hotmail.com>
2022-10-03 14:28:30 -0300

    criando testes

--- tests/test_triangulo.py
+++ tests/test_triangulo.py
@@ -5,5 +5,16 @@ from codigo.Triangulo import triangle_type
 def test_equilatero(): #caso de teste
         assert triangle_type(3,3,3) == "Equilateral"
 
+def test_escaleno(): #caso de teste
+        assert triangle_type(10,9,12) == "Scalene"
+
+def test_isosceles(): #caso de teste
+        assert triangle_type(10,10,12) == "Isosceles"
+
+
+def test_invalid(): #caso de teste
+        assert triangle_type(10,10,21) == "Not a triangle"
+
+
 
 
